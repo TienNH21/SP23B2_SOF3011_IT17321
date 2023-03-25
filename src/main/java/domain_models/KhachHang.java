87e291995f481a9da15754e1301fce6dd06cc2e6
HoangTien339 <tiennh21@fpt.edu.vn>
2023-03-25 11:10:09 +0700

    QLKH: ORM - JPA/Hibernate & Entity

---
+++ src/main/java/domain_models/KhachHang.java
@@ -0,0 +1,149 @@
+package domain_models;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name="KhachHang")
+public class KhachHang {
+    @Id
+    @Column(name="Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String Id;
+
+    @Column(name="Ma")
+    private String Ma;
+
+    @Column(name="Ten")
+    private String Ten;
+
+    @Column(name="TenDem")
+    private String TenDem;
+
+    @Column(name="Ho")
+    private String Ho;
+
+    @Column(name="NgaySinh")
+    private Date NgaySinh;
+
+    @Column(name="Sdt")
+    private String Sdt;
+
+    @Column(name="DiaChi")
+    private String DiaChi;
+
+    @Column(name="ThanhPho")
+    private String ThanhPho;
+
+    @Column(name="QuocGia")
+    private String QuocGia;
+
+    @Column(name="MatKhau")
+    private String MatKhau;
+
+    public KhachHang(String id, String ma, String ten, String tenDem, String ho, Date ngaySinh, String sdt, String diaChi, String thanhPho, String quocGia, String matKhau) {
+        Id = id;
+        Ma = ma;
+        Ten = ten;
+        TenDem = tenDem;
+        Ho = ho;
+        NgaySinh = ngaySinh;
+        Sdt = sdt;
+        DiaChi = diaChi;
+        ThanhPho = thanhPho;
+        QuocGia = quocGia;
+        MatKhau = matKhau;
+    }
+
+    public KhachHang() {
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getMa() {
+        return Ma;
+    }
+
+    public void setMa(String ma) {
+        Ma = ma;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String ten) {
+        Ten = ten;
+    }
+
+    public String getTenDem() {
+        return TenDem;
+    }
+
+    public void setTenDem(String tenDem) {
+        TenDem = tenDem;
+    }
+
+    public String getHo() {
+        return Ho;
+    }
+
+    public void setHo(String ho) {
+        Ho = ho;
+    }
+
+    public Date getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        NgaySinh = ngaySinh;
+    }
+
+    public String getSdt() {
+        return Sdt;
+    }
+
+    public void setSdt(String sdt) {
+        Sdt = sdt;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        DiaChi = diaChi;
+    }
+
+    public String getThanhPho() {
+        return ThanhPho;
+    }
+
+    public void setThanhPho(String thanhPho) {
+        ThanhPho = thanhPho;
+    }
+
+    public String getQuocGia() {
+        return QuocGia;
+    }
+
+    public void setQuocGia(String quocGia) {
+        QuocGia = quocGia;
+    }
+
+    public String getMatKhau() {
+        return MatKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        MatKhau = matKhau;
+    }
+}
