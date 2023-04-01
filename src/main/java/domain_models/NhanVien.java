0ba4911d3acea611acdb6923ee085a26cfd40548
HoangTien339 <tiennh21@fpt.edu.vn>
2023-04-01 11:04:57 +0700

    Relation Mapping

---
+++ src/main/java/domain_models/NhanVien.java
@@ -0,0 +1,170 @@
+package domain_models;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name="NhanVien")
+public class NhanVien {
+    @Id
+    @Column(name="Id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name="Ma")
+    private String ma;
+
+    @Column(name="Ho")
+    private String ho;
+
+    @Column(name="TenDem")
+    private String tenDem;
+
+    @Column(name="Ten")
+    private String ten;
+
+    @Column(name="GioiTinh")
+    private String gioiTinh;
+
+    @Column(name="NgaySinh")
+    private Date ngaySinh;
+
+    @Column(name="DiaChi")
+    private String diaChi;
+
+    @Column(name="Sdt")
+    private String sdt;
+
+    @Column(name="MatKhau")
+    private String matKhau;
+
+    @ManyToOne()
+    @JoinColumn(name="IdCV")
+    private ChucVu cv;
+
+    @Column(name="IdCH")
+    private UUID idCH;
+
+    @Column(name="IdGuiBC")
+    private UUID idGuiBC;
+
+    @Column(name="TrangThai")
+    private Integer trangThai;
+
+    public NhanVien() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTenDem() {
+        return tenDem;
+    }
+
+    public void setTenDem(String tenDem) {
+        this.tenDem = tenDem;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public ChucVu getCv() {
+        return cv;
+    }
+
+    public void setCv(ChucVu cv) {
+        this.cv = cv;
+    }
+
+    public UUID getIdCH() {
+        return idCH;
+    }
+
+    public void setIdCH(UUID idCH) {
+        this.idCH = idCH;
+    }
+
+    public UUID getIdGuiBC() {
+        return idGuiBC;
+    }
+
+    public void setIdGuiBC(UUID idGuiBC) {
+        this.idGuiBC = idGuiBC;
+    }
+
+    public Integer getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(Integer trangThai) {
+        this.trangThai = trangThai;
+    }
+}
--- src/main/java/utils/HibernateUtil.java
+++ src/main/java/utils/HibernateUtil.java
@@ -1,12 +1,17 @@
 package utils;
 
+import domain_models.ChucVu;
 import domain_models.KhachHang;
+import domain_models.NhanVien;
+import jakarta.persistence.TypedQuery;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
+import java.util.List;
 import java.util.Properties;
 
 public class HibernateUtil {
@@ -25,6 +30,8 @@ public class HibernateUtil {
 
         conf.setProperties(properties);
         conf.addAnnotatedClass(KhachHang.class);
+        conf.addAnnotatedClass(ChucVu.class);
+        conf.addAnnotatedClass(NhanVien.class);
         ServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .applySettings(conf.getProperties()).build();
         FACTORY = conf.buildSessionFactory(registry);
@@ -36,7 +43,17 @@ public class HibernateUtil {
     }
 
     public static void main(String[] args) {
-        System.out.println(getFACTORY());
+        Session hSession = getFACTORY().openSession();
+        String ma = "1";
+        String hql = "SELECT cvObj FROM ChucVu cvObj WHERE cvObj.ma = ?1";
+        TypedQuery<ChucVu> query = hSession.createQuery(hql, ChucVu.class);
+        query.setParameter(1, ma);
+        ChucVu cv = query.getSingleResult();
+        System.out.println(cv.getTen());
+
+        List<NhanVien> listNv = cv.getListNv();
+        NhanVien nv = listNv.get(0);
+        System.out.println(nv.getHo() + " " + nv.getTenDem() + " " + nv.getTen());
     }
 
 }
